b36068d9fc2098e67d4f0c83c33918cfb3ea703b
Shital Shah <shitals@microsoft.com>
2023-01-29 01:33:07 -0800

    multi regression working

--- multi_regr.py
+++ multi_regr.py
@@ -2,17 +2,23 @@ import numpy as np
 import torch
 from torch import nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split, Subset
+from torchsummary import summary
 
 from autoregressive_dataset import AutoregressiveDataset
 
 from regression_model import RegressionModel
+import random
 
+random.seed(42)
 torch.manual_seed(42)
 
-def generate_dataset(window_size=500, dataset_size=100000, train_split=0.5):
+def generate_dataset(window_size=500, dataset_size=100000):
     A = torch.rand((window_size, window_size), requires_grad=False)*2.0-1.0
     x = torch.rand((window_size, 1), requires_grad=False)*2.0-1.0
+
+    A = A.cuda()
+    x = x.cuda()
 
     dataset = []
     while len(dataset) < dataset_size:
@@ -22,34 +28,49 @@ def generate_dataset(window_size=500, dataset_size=100000, train_split=0.5):
         dataset.append(x1.item())
         x = torch.cat((x[1:], x1.unsqueeze(0)))
 
-    train_size = int(dataset_size * train_split)
-    test_size = len(dataset) - train_size
-
-    return dataset[:train_size], dataset[test_size:]
+    return dataset
 
 
-def eval_model(model:RegressionModel, test_data:list[float], batch_size):
+def eval_model(model:RegressionModel, test_dataloader:DataLoader):
     model.eval()
-    test_dataset = AutoregressiveDataset(window_size=model.model_size, sequence=test_data)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     loss_function = nn.MSELoss().cuda()
 
-    total_loss = 0.0
+    total_loss, data_count = 0.0, 0
     for x, y in test_dataloader:
         x = x.cuda()
         y = y.cuda().unsqueeze(1)
         y_pred = model(x)
         loss = loss_function(y_pred, y)
         total_loss += loss.item() * len(x)
+        data_count += len(x)
 
-    return total_loss / len(test_dataset)
+    return total_loss / data_count
 
-def train_model(train_data:list[float], test_data:list[float], model_size=500, epochs=10, batch_size=4098):
+def split_ds(dataset, train_split=0.5, val_split=0.1):
+    train_size = int(train_split * len(dataset))
+    val_size = int(val_split * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+    train_indices = list(range(train_size))
+    val_indices = list(range(train_size, train_size + val_size))
+    test_indices = list(range(train_size + val_size, train_size + val_size + test_size))
+
+    train_dataset = Subset(dataset, train_indices)
+    val_dataset = Subset(dataset, val_indices)
+    test_dataset = Subset(dataset, test_indices)
+
+    return train_dataset, val_dataset, test_dataset
+
+def train_model(ds:list[float], train_split=0.5, val_split=0.1, model_size=500, epochs=10, batch_size=4098):
     model = RegressionModel(model_size=model_size).cuda()
-    optimizer = optim.Adam(model.parameters(), lr=1e-2)
+    optimizer = optim.Adam(model.parameters(), lr=0.5)
     loss_function = nn.MSELoss().cuda()
-    train_dataset = AutoregressiveDataset(window_size=model_size, sequence=train_data)
+
+    dataset = AutoregressiveDataset(window_size=model_size, sequence=ds)
+    train_dataset, val_dataset, test_dataset = split_ds(dataset, train_split, val_split)
+
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     loss_curve = []
     step = 0
@@ -67,15 +88,20 @@ def train_model(train_data:list[float], test_data:list[float], model_size=500, e
 
             step += 1
             if step % 1000:
-                curve_point = (epoch, step, loss.item()/len(x), eval_model(model, test_data, batch_size))
+                curve_point = (epoch, step, loss.item(), eval_model(model, val_dataloader))
                 loss_curve.append(curve_point)
                 print(curve_point)
 
-    return loss_curve
+    return loss_curve, eval_model(model, test_dataloader), model
 
 
 if __name__ == "__main__":
-    train_data, test_data = generate_dataset(window_size=500, dataset_size=100000)
-    train_model(train_data, test_data, model_size=500, epochs=20, batch_size=8192)
+    model_size = 200
+    ds = generate_dataset(window_size=500, dataset_size=10000)
+    print("dataset stats: ", np.mean(ds), np.std(ds), np.min(ds), np.max(ds))
+    print("dataset sample: ", random.sample(ds, 10))
+    loss_curve, test_loss, model = train_model(ds, model_size=model_size, epochs=20, batch_size=8192)
+    print("params=", sum(p.numel() for p in model.parameters()))
+    print("test_loss=",test_loss)
 
 
--- regression_model.py
+++ regression_model.py
@@ -5,7 +5,7 @@ class RegressionModel(nn.Module):
   def __init__(self, model_size:int):
     super().__init__()
     self.model_size = model_size
-    self.A = nn.Parameter(torch.FloatTensor(1,model_size))
+    self.A = nn.Parameter(torch.randn(1,model_size))
 
   def forward(self, x):
     return self.A.mm(x.t()).t()
